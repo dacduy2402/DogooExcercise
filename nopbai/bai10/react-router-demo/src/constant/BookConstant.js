c9e483aadeafd85bec46bb9959639eb2460352e7
khoavu <khoavu@khoavus-MacBook-Pro.local>
2021-09-20 07:55:54 +0700

    Update bai11

--- nopbai/bai10/react-router-demo/src/constant/BookConstant.js
+++
@@ -1,8 +0,0 @@
-
-export const BookConstant = {
-
-    BOOK_BASE_URL : '  http://localhost:8080/v1/api/books',
-    GET_DETAIL_BOOK : 'http://localhost:8080/v1/api/books/?id=',
-    CATEGORIES_BASE_URL : 'http://localhost:8080/v1/api/categories',
-    PAGINATION_PAGE : 'http://localhost:8080/v1/api/books?'
-}
